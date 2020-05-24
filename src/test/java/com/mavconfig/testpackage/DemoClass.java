f4e8564477a483a6af437c2be0b07f927d60921a
ArunDrioder <arunprasadh.s@gmail.com>
2020-05-24 23:20:40 +0530

    Initial commit, With added standard java class for reading data from excel, and also my first maven project commit

---
+++ src/test/java/com/mavconfig/testpackage/DemoClass.java
@@ -0,0 +1,48 @@
+package com.mavconfig.testpackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoClass
+{
+    @Test
+    public void addTwoNum()
+    {
+        System.out.println("Performing Addition Operation");
+        int a = 25;
+        int b = 26;
+        Assert.assertEquals(51,a+b);
+
+    }
+
+    @Test
+    public void subTwoNum()
+    {
+        System.out.println("Performing Subtraction Operation");
+        int a = 25;
+        int b = 26;
+        Assert.assertEquals(1,b-a);
+
+    }
+
+    @Test
+    public void multTwoNum()
+    {
+        System.out.println("Performing multiplication Operation");
+        int a = 25;
+        int b = 26;
+        Assert.assertEquals(650,a*b);
+
+    }
+
+    @Test
+    public void divTwoNum()
+    {
+        System.out.println("Performing Division Operation");
+        int a = 25;
+        int b = 26;
+        Assert.assertEquals(1,b/a);
+
+    }
+
+}
