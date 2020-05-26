89294958ba6f961ce920dbbe20ebdaa19771a742
ArunDrioder <arunprasadh.s@gmail.com>
2020-05-26 18:51:58 +0530

    Completed the first phase of Data driven test by using excel sheets for data -- > (Used only Static Data), So the class has been renamed to StaticDataDrivenClass., In case of referring in future, ignore the previous commit on the same day. and directly refer this commit.,
    The Excel has been modified with Dynamic data. It's not included in this commit. It'll be included in the next commit along with the script for the dynamic fetching of data from Excel

--- src/test/java/com/datadriventest/StaticDataDrivenClass.java
+++ src/test/java/com/datadriventest/StaticDataDrivenClass.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 
-public class DataDrivenClass
+public class StaticDataDrivenClass
 
 {
     WebDriver driver;
